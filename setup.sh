bb43f6b0b4b581ed41e58fec22ce11741c8a1583
Mathieu Comandon <strider@strycore.com>
2011-07-06 12:33:27 +0200

    nicer urxvt with inconsolata

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
+set -e
 
-cp ./bashrc $HOME/.bashrc
-cp ./screenrc $HOME/.screenrc
-cp ./vimrc $HOME/.vimrc
-
-
+ln -s $(pwd)/bashrc $HOME/.bashrc
+ln -s $(pwd)/screenrc $HOME/.screenrc
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/vim $HOME/.vim
 
