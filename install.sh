89c83e56f9f23877e16cd2060564b0093e52abd1
Mathieu Comandon <strycore@gmail.com>
2012-08-22 02:08:54 +0200

    working install script

--- install.sh
+++ install.sh
@@ -3,18 +3,26 @@ function link_file {
     source="${PWD}/$1"
     target="${HOME}/.${1}"
 
-    if [ -e "${target}" ]; then
-        mv $target $target.bak
+    if [ -e "${target}" ]
+    then
+        if [ $(readlink ${target}) == ${source} ]
+        then
+            echo "[SKIP] Existing target ${target}, skipping file $1"
+            return
+        else 
+            backup=$target.backup$(date +%s)
+            echo "[BCKP] Saving backup of exising file ${target} as ${backup}"
+            mv $target $backup
+        fi
     fi
-
+    echo "[ OK ] Creating link to ${source}"
     ln -sf ${source} ${target}
 }
 
-
-git submodule sync
-git submodule init
-git submodule update
-git submodule foreach git pull origin master
-git submodule foreach git submodule init
-git submodule foreach git submodule update
+link_file vim
+link_file vimrc
+link_file zshrc
+link_file screenrc
+link_file pylintrc
+link_file gitconfig
 
--- update.sh
+++ update.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-function link_file {
-    source="${PWD}/$1"
-    target="${HOME}/.${1}"
-
-    if [ -e "${target}" ]; then
-        mv $target $target.bak
-    fi
-
-    ln -sf ${source} ${target}
-}
-
 
 git submodule sync
 git submodule init
