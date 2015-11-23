15177e3c577c2c0159ab6debfdac2b83af6f3f4e
Mathieu Comandon <strycore@gmail.com>
2015-11-22 16:47:11 -0800

    Add node-setup script

---
+++ node-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NODE_CURRENT="v5.1.0"
+NVM_DIR="$HOME/.nvm"
+
+if [ ! -d "$NVM_DIR" ]; then
+    echo "nvm is not installed"
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+fi
+
+source $NVM_DIR/nvm.sh
+
+nvm install $NODE_CURRENT
+nvm alias default $NODE_CURRENT
+nvm use default
+
+npm install grunt-cli -g
+npm install gulp -g
+npm install browser-sync -g
+npm install eslint -g
