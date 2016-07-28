2c4e5cf1f9c56eaf9ebeb0869cf17a3952e35d2f
Mathieu Comandon <strycore@gmail.com>
2016-07-28 11:55:26 -0700

    Update Node and NVM

--- node-setup.sh
+++ node-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-NODE_CURRENT="v6.2.1"
+NODE_CURRENT="v6.3.1"
 NVM_DIR="$HOME/.nvm"
 
 if [ ! -d "$NVM_DIR" ]; then
     echo "Installing NVM"
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.3/install.sh | bash
 fi
 
 source $NVM_DIR/nvm.sh
