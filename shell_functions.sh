e0e469676d893e3797906d3a17d4a330de609049
Mathieu Comandon <strycore@gmail.com>
2016-09-27 10:05:47 -0700

    Add function to fetch IRC logs

--- shell_functions.sh
+++ shell_functions.sh
@@ -1,4 +1,4 @@
-function cron_environ () 
+function cron_environ ()
 {
     # Export Gnome Keyring and SSH agent environment variables to a file so they
     # can be used by cron jobs.
@@ -9,4 +9,12 @@ function cron_environ ()
     echo 'export DBUS_SESSION_BUS_ADDRESS' >> $CRON_ENVIRON
     env | grep SSH_AUTH_SOCK >> $CRON_ENVIRON
     echo 'export SSH_AUTH_SOCK' >> $CRON_ENVIRON
+}
+
+function irc_log() {
+    channel=$1
+    today=$(date "+%Y-%m-%d")
+    hostname="irc.strycore.com"
+    logpath="/home/strider/.znc/moddata/log/strider/default/"
+    scp "$hostname:$logpath#$channel/$today.log" "#${channel}-${today}.log"
 }
