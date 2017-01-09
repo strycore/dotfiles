6d6795a3ab233d4ce690db4a3e50a31f14e9f0fb
Mathieu Comandon <strycore@gmail.com>
2017-01-09 11:40:46 -0800

    Update font install script to use awesome terminal fonts

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-git clone https://github.com/powerline/fonts.git powerline-fonts
-cd powerline-fonts
-./install.sh
-cd ..
-rm -rf powerline-fonts
+mkdir -p ~/.fonts
+
+# git clone https://github.com/powerline/fonts.git powerline-fonts
+# cd powerline-fonts
+# ./install.sh
+# cd ..
+# rm -rf powerline-fonts
+
+git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
+cd awesome-terminal-fonts
+git checkout patching-strategy
+cd patched
+cp *.ttf ~/.fonts
+cd ../..
+rm -rf awesome-terminal-fonts
