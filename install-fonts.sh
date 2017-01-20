3686dab32007a582a7ce4131331a57deb6f4c313
Mathieu Comandon <strycore@gmail.com>
2017-01-20 15:38:16 -0800

    Still use powerline fonts in vim

--- install-fonts.sh
+++ install-fonts.sh
@@ -2,11 +2,11 @@
 
 mkdir -p ~/.fonts
 
-# git clone https://github.com/powerline/fonts.git powerline-fonts
-# cd powerline-fonts
-# ./install.sh
-# cd ..
-# rm -rf powerline-fonts
+git clone https://github.com/powerline/fonts.git powerline-fonts
+cd powerline-fonts
+./install.sh
+cd ..
+rm -rf powerline-fonts
 
 git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
 cd awesome-terminal-fonts
