594ab9963b2fffb0d81fb36f439944d159bc49b0
Mathieu Comandon <strycore@gmail.com>
2018-10-04 00:29:20 -0700

    Add gnome-mpv audcious and codecs to post install

--- post-install.sh
+++ post-install.sh
@@ -50,7 +50,8 @@ sudo apt --yes install git keepassxc apt-file pavucontrol htop gimp chrome-gnome
     winehq-staging gimp inkscape lutris virtualbox-5.2 tilix zsh cmake dconf-editor gnome-tweaks \
     gconf-service gconf-service-backend gconf2-common libappindicator1 libc++1 libc++abi1 libgconf-2-4 libindicator7 \
     gnome-shell-extension-pop-suspend-button pop-fonts pop-gnome-shell-theme pop-gtk-theme pop-icon-theme python-apt \
-    libsystemd-dev vulkan-utils chromium-browser picard
+    libsystemd-dev vulkan-utils chromium-browser picard gnome-mpv audacious gstreamer1.0-plugins-bad gstreamer1.0-plugins-good \
+    gstreamer1.0-plugins-ugly
 
 if [ ! -e /opt/google/chrome ]; then
     # Install Chrome
